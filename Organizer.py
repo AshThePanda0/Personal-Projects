915de38740236cca5e2f62035c37a21c082c405b
AshThePanda0 <anbutler@loyola.edu>
2025-09-13 11:14:54 -0400

    Beginning Of organizer

---
+++ Organizer.py
@@ -0,0 +1,17 @@
+#Ask user how many assignments
+print("How many assignments: ")
+n = int(input())
+# Code block to repeat a string n times
+for _ in range(n):
+    # Data for assignment one
+    print("Name of assignment: ")
+    assign_name = str(input())
+    assign_date = int(input("Day ")), int(input("Month ")), int(input("Year "))
+    print(assign_name, assign_date)
+
+
+
+
+
+
+
